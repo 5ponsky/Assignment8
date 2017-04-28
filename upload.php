5154217ef5dbff845b0c1428132ab60cfd4819ba
Liam Dillingham <lgdillin@uark.edu>
2017-04-28 15:01:15 -0500

    4/28/17 3:00 PM
    - File Delte works
    
    ==> requires a 2nd refresh to show deleted files

--- upload.php
+++ upload.php
@@ -1,5 +1,4 @@
 <?php session_start(); ?>
-<?php print_r($_POST); ?>
 
 <!DOCTYPE html>
 
@@ -24,18 +23,30 @@
       ini_set("display_errors", 1);
       error_reporting(~0);
 
+      // Prevent execution if no file has been selected
+      if(!isset($_FILES["fileToUpload"]["name"])) {
+        print("no file chosen\n");
+      }
+
       $target_dir = "uploads/";
       $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
       $uploadOk = 1;
       $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
-      // initialize the session token
+      // Scan the uploads folder for new files
+
+
+      // Initialize the session token
       if(isset($_SESSION["file_count"])) {
         $_SESSION["file_count"] = 0;
       }
 
       // Check if the image file is a real image file
       if(isset($_POST["submit"])) {
+        // Prevent execution if no file has been selected
+        if(!$_FILES["fileToUpload"]["tmp_name"]) {
+          print("No file chosen!\n");
+        }
 
         // This hack abuses an image size function
         //if the function does not error out, then the file is an image
@@ -45,30 +56,23 @@
           echo "File is an image - " . $check["mime"] . ".";
           $uploadOk = 1;
         } else {
-          echo "File is not an image";
+          echo "File is not an image\n";
           $uploadOk = 0;
         }
       }
 
       // Check if the file already exists
       if(file_exists($target_file)) {
-        echo "File already exists";
+        echo "File already exists\n";
         $uploadOk = 0;
       }
 
       // Check file Size
-      $fileSize = 500000;
+      $fileSize = 500000000;
       if($_FILES["fileToUpload"]["size"] > $fileSize) {
-        echo "File size exceeds maximum size";
+        echo "File size exceeds maximum size\n";
         $uploadOk = 0;
       }
-
-      // Only allow certain file types
-      if($imageFileType != "jpg" && $imageFIleType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif") {
-          echo "Sorry, invalid file type";
-          $uploadOk = 0;
-        }
 
       // Check if the $uploadOk flag has been dropped by an error ($uploadOk == 0)?
       if($uploadOk == 0) {
@@ -82,7 +86,7 @@
 
           echo "The file: " . basename($_FILES["fileToUpload"]["name"]). " has been uploaded";
         } else {
-          echo "Sorry, there was an error uploading your file";
+          echo "Sorry, there was an error uploading your file\n";
         }
       }
      ?>
@@ -90,7 +94,7 @@
     <br>
     <br>
 
-    <form method="post" id="userform" action="thisform.php">
+    <form action="upload.php" method="post" enctype="multipart/form-data" id="userform">
       <table>
         <tr style="border: 1px solid black; align: center;">
           <td style="border: 1px solid black; align: center;">Flag</td>
@@ -98,26 +102,28 @@
         </tr>
 
         <?php
-          $files = array_slice(scandir($target_dir), 2);
-          $size = count($files);
-          for($i = 0; $i < $size; $i++) {
-            print("<tr><td>" . $files[$i] . "</td></tr>\n");
-          }
+        $files = array_slice(scandir($target_dir), 2);
+        $size = count($files);
+        for($i = 0; $i < $size; $i++) {
+          print("<tr><td><a href='" . $target_dir . $files[$i] . "' target='_blank'>" . $files[$i] . "</a></td><td><input type='checkbox' name='checkboxvar[]' value='" . $files[$i] . "'></td></tr>\n");
+        }
         ?>
-        <tr>
-          <td>
-            <input type="checkbox" name="checkboxvar[]" value="Option One">1<br>
-            <input type="checkbox" name="checkboxvar[]" value="Option Two">2<br>
-            <input type="checkbox" name="checkboxvar[]" value="Option Three">3<br>
-          </td>
-        </tr>
       </table>
 
+      <input type="submit" value="Delete Flagged" name="delete">
     </form>
 
     <?php
       if (isset($_POST['checkboxvar'])) {
+        //array_map('unlink', glob($target_dir . '/' . $_POST['checkboxvar']));
         print_r($_POST['checkboxvar']);
+        foreach($_POST['checkboxvar'] as $it) {
+          unlink($target_dir . $it);
+        }
+        //chown($target_dir . '/' . $_POST['checkboxvar'], 666);
+        //unlink($target_dir . '/' . $_POST['checkboxvar']);
+        //unlink($_POST['checkboxvar']);
+        //print_r($_POST['checkboxvar']);
       }
     ?>
 
